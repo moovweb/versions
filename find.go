25885a7394c9b20e18ec0eef56122e339dade377
Jose Espinosa <jose.espinosa@moovweb.com>
2012-06-28 16:26:29 -0700

    Add Find* files that support Patterns

--- find.go
+++ find.go
@@ -12,10 +12,24 @@ type FilePath struct {
 }
 
 func FindByName(rootPath string, name string) (*FilePath, error) {
-	return FindByNameLatestVersion(rootPath, name, "");
+	return FindByNameAndVersionLatest(rootPath, name, "")
 }
 
-func FindByNameLatestVersion(rootPath string, name string, versionPattern string) (*FilePath, error) {
+func FindByNameAndPatternLatest(rootPath string, name string, versionPattern *Pattern) (*FilePath, error) {
+	filePaths, err := FindByNameAndPattern(rootPath, name, versionPattern)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(filePaths) == 0 {
+		return nil, errors.New("Found no files named " + name + " in path: " + rootPath)
+	}
+
+	return getLatestVersion(filePaths), nil
+}
+
+func FindByNameAndVersionLatest(rootPath string, name string, versionPattern string) (*FilePath, error) {
 	filePaths, err := FindByNameAndVersion(rootPath, name, versionPattern)
 
 	if err != nil {
@@ -26,6 +40,10 @@ func FindByNameLatestVersion(rootPath string, name string, versionPattern string
 		return nil, errors.New("Found no files named " + name + " in path: " + rootPath)
 	}
 
+	return getLatestVersion(filePaths), nil
+}
+
+func getLatestVersion(filePaths []*FilePath) *FilePath {
 	newestFilePath := filePaths[0]
 
 	for _, thisFilePath := range filePaths {
@@ -36,7 +54,7 @@ func FindByNameLatestVersion(rootPath string, name string, versionPattern string
 		}
 	}
 
-	return newestFilePath, nil
+	return newestFilePath
 }
 
 func FindByNameAndVersion(rootPath string, name string, versionPattern string) (paths []*FilePath, err error) {
@@ -60,6 +78,33 @@ func FindByNameAndVersion(rootPath string, name string, versionPattern string) (
 		}
 
 		if matched {
+			matchingPath := &FilePath{
+				Path:    result,
+				Name:    name,
+				Version: version,
+			}
+			paths = append(paths, matchingPath)
+		}
+	}
+
+	return
+}
+
+func FindByNameAndPattern(rootPath string, name string, versionPattern *Pattern) (paths []*FilePath, err error) {
+	// TODO(JE): Refactoring to share the same code in this function and FindByNameAndVersion
+	query := filepath.Join(rootPath, name) + "*"
+	results, _ := filepath.Glob(query)
+
+	for _, result := range results {
+		version, err := GetVersion(result)
+
+		if err != nil {
+			println("Didnt get valid version from:" + result)
+			println(err.Error())
+			continue
+		}
+
+		if versionPattern.Match(version) {
 			matchingPath := &FilePath{
 				Path:    result,
 				Name:    name,
